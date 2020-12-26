c2e9efecbbc4fb9f681a73f68ff4d597fdb3d346
zhaohualuo <a2622080214@qq.com>
2020-12-27 00:00:03 +0800

    add a topic:83. 删除排序链表中的重复元素

---
+++ src/main/java/com/harley/leetcode/api/ListNode.java
@@ -0,0 +1,10 @@
+package com.harley.leetcode.api;
+
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+}
---
+++ src/main/java/com/harley/leetcode/api/No83.java
@@ -0,0 +1,32 @@
+package com.harley.leetcode.api;
+
+/**
+ * 83. 删除排序链表中的重复元素
+ * 给定一个排序链表，删除所有重复的元素，使得每个元素只出现一次。
+ */
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class No83 {
+    public ListNode deleteDuplicates(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+        ListNode result = head;
+        while (result.next != null) {
+            if (result.val == result.next.val) {
+                result.next = result.next.next;
+            } else {
+                result = result.next;
+            }
+
+        }
+        return head;
+    }
+
+}
